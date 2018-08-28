63cab7cece25fa2e9b253cfb47a2788490a0ba4f
root <root@localhost.localdomain>
2018-08-28 11:12:24 -0400

    Updating java install

--- tools/install-java.sh
+++ tools/install-java.sh
@@ -1,5 +1,5 @@
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
