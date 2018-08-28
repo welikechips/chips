4241d6189ce8382948b11eeaf0ca5eade711539f
root <root@localhost.localdomain>
2018-08-28 11:37:18 -0400

    Adding gnome terminal

--- 2nd.sh
+++ 2nd.sh
@@ -16,7 +16,7 @@ git clone https://github.com/tnory56/.tmux.git
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-tmux new -s tmux-setup
+gnome-terminal -- tmux new -s tmux-setup
 echo "Press prefix + r to reload the tmux config"
 echo "Press prefix + I to install the plugins"
 read -p "Press enter when done with TMUX setup"
---
+++ test.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- tmux new -s tmux-setup
+echo "Press prefix + r to reload the tmux config"
+echo "Press prefix + I to install the plugins"
+read -p "Press enter when done with TMUX setup"
+
