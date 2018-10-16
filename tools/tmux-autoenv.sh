c3748e2fc76f04c4970192595895798ed55ada0e
root <root@localhost.localdomain>
2018-10-16 09:52:24 -0400

    Updating tmux autoenv.sh

--- tools/tmux-autoenv.sh
+++ tools/tmux-autoenv.sh
@@ -1,3 +1,6 @@
-tmux setenv WD ${1}
-export WD=${1}
-echo "Your working directory is $WD"
+#!/bin/bash
+tmux setenv IP ${1}
+tmux setenv WD $(pwd)
+export IP=${1}
+export WD=$(pwd)
+echo "Current IP Address is ${IP} and current working directory is ${WD}"
