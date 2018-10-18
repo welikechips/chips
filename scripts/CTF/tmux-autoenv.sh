c6534723feb036b20cc40e5c0386a32bfc4788e1
root <root@localhost.localdomain>
2018-10-18 15:21:51 -0400

    chmod

--- scripts/CTF/tmux-autoenv.sh
+++ scripts/CTF/tmux-autoenv.sh
@@ -7,5 +7,5 @@ tmux setenv IP ${IP}
 tmux setenv WD $(pwd)
 export IP=${IP}
 export WD=$(pwd)
-sudo echo "${IP}    ${server_name}" > /etc/hosts
+sudo echo "${IP}    ${server_name}" >> /etc/hosts
 echo "Current IP Address is ${IP}, server name is ${server_name} and current working directory is ${WD}"
