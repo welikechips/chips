a2e0e7558f8e7df283bc0a588c706de80e07a343
weirdatfirst <weirdatfirst@gmail.com>
2018-10-17 21:01:58 -0400

    Moved pycharm to /opt

--- scripts/pycharminstall.sh
+++ scripts/pycharminstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 pycharm=/pycharm-community-2018.2.4/bin/pycharm.sh
-cd ../tools
+cd /opt/
 wget www.weirdatfirst.com/files/tools/pycharm-community-2018.2.4.tar.gz
 path=$(pwd)
 tar -xf pycharm-community-2018.2.4.tar.gz
 cd /usr/bin/
 ln -s $path$pycharm pycharm
+rm /opt/pycharm-community-2018.2.4.tar.gz
 
 
 
