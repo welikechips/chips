43ae3de5e979478a03eb471c3c31ae284dd8ccee
weirdatfirst <weirdatfirst@gmail.com>
2018-10-31 11:49:45 -0400

    added HTMLPrettify to Sublime

--- scripts/Utilities/sublime.sh
+++ scripts/Utilities/sublime.sh
@@ -4,3 +4,4 @@ sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-ke
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get -y install sublime-text
+git clone https://github.com/victorporof/Sublime-HTMLPrettify.git ~/.config/sublime-text-3/Packages/Sublime-HTMLPrettify
