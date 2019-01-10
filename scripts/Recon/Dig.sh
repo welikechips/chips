4dc8a5d80ad48631c33daaaa6c468b9e25862fd1
weirdatfirst <weirdatfirst@gmail.com>
2019-01-09 22:03:46 -0500

    adding dig.sh

---
+++ scripts/Recon/Dig.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+path="/root/scans/"
+declare -a opt_name=()
+declare -A opt_file=()
+i=0
+while read line
+do
+	opt_name[ $i ]=$(basename "${line}")
+	opt_file[${opt_name[$i]}]="$line"
+	(( i++ ))
+done < <(ls $path)
+
+PS3='Which domain do you want to dig? (q to quit): '
+select name in "${opt_name[@]}"; do
+	case "$name" in
+		"") break ;;
+		*) 	domain=${opt_file[$name]}
+			dig -f $path$domain/sublist.txt > $path/$domain/dig.txt
+			echo "Dig File Written!"
+			exit;;
+	esac
+done
--- scripts/Recon/Sublist3r.sh
+++ scripts/Recon/Sublist3r.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
 echo "Right now the output file will be in /root/scans/"
 echo "What domain do you want to scan?"
-
 read domain
-mkdir -p /root/scans/$domain
-python /root/tools/Sublist3r/sublist3r.py -d $domain -o /root/scans/$domain/sublist.txt
+
+path="/root/scans/$domain/"
+mkdir -p $path
+python /root/tools/Sublist3r/sublist3r.py -d $domain -o $path/sublist.txt
+
 
 
 
