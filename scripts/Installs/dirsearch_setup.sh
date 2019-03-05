00700c758c6c74eb083824562ec07dc7d2ee79b6
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-05 16:18:17 -0500

    spellcheck

--- scripts/Installs/dirsearch_setup.sh
+++ scripts/Installs/dirsearch_setup.sh
@@ -4,4 +4,4 @@ mkdir /root/tools
 cd /root/tools/
 git clone https://github.com/maurosoria/dirsearch.git
 cd /usr/bin/
-ln -s /root/tools/dirseach/dirseach.py dirsearch
+ln -s /root/tools/dirsearch/dirsearch.py dirsearch
