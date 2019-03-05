d525d53c5433c39f8b953e4c4bdeb7dd84498a20
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-05 17:01:09 -0500

    add folder check to sublist3r

--- scripts/Recon/dirsearch_fromfile.sh
+++ scripts/Recon/dirsearch_fromfile.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Description: DirSearch - Run Multiple URLS
-path=$(pwd)
-read -r -p 'Please enter full path of file ' file
-mkdir $path/dirsearch
-name=$(cat "$file")
-for i in $name; do
-        echo https://$i
-        dirsearch -u https://$i -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt,asp,aspx -x 403 --simple-report=dirsearch/$i
-done
+
+if [ ! -d "/root/tools/dirsearch" ]; then
+    echo "Make sure you run dirsearch Install"
+else
+    path=$(pwd)
+    read -r -p 'Please enter full path of file ' file
+    mkdir $path/dirsearch
+    name=$(cat "$file")
+    for i in $name; do
+            echo https://$i
+            dirsearch -u https://$i -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt,asp,aspx -x 403 --simple-report=dirsearch/$i
+    done
+fi
