d3380f8e6aa2d8693eeedc075e2405afa19a8a3c
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-05 16:57:11 -0500

    add folder check to dirsearch

--- scripts/Recon/sublist3r.sh
+++ scripts/Recon/sublist3r.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Description: Sublist3r - Run
-echo "Right now the output file will be in /root/scans/"
-echo "What domain do you want to scan?"
-read domain
 
-path="/root/scans/$domain/"
-mkdir -p $path
-python /root/tools/Sublist3r/sublist3r.py -d $domain -o $path/sublist.txt
-
-
-
-
+if [ ! -d "/root/tools/dirsearch" ]; then
+    echo "Make sure you run dirsearch Install"
+else
+    echo "Right now the output file will be in /root/scans/"
+    echo "What domain do you want to scan?"
+    read domain
+    path="/root/scans/$domain/"
+    mkdir -p $path
+    python /root/tools/Sublist3r/sublist3r.py -d $domain -o $path/sublist.txt
+fi
