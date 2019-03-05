bad079c92f0e1a2ee1827e2db2ca1e6721d7acfe
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-05 16:53:26 -0500

    Updated eyewitness

--- scripts/Recon/eyewitness.sh
+++ scripts/Recon/eyewitness.sh
@@ -3,7 +3,6 @@
 if [ ! -d "/root/tools/EyeWitness" ]; then
     echo "Make sure you run EyeWitness Install"
 else
-    echo "Make sure you ran the install"
     read -r -p 'Please enter full path of file ' filename
     eyewitness -f $filename --web
 fi
