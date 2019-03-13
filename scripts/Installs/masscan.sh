3623222c749a2af3491ae36afc7f992594a1e421
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-13 12:39:47 -0400

    removing masscan. already default on Kali

--- scripts/Installs/masscan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Description: Masscan
-mkdir -p /tools/
-cd tools
-sudo apt-get install git gcc make libpcap-dev -y
-git clone https://github.com/robertdavidgraham/masscan
-cd masscan
-make
