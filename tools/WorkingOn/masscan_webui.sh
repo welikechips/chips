e174a5f4fde3e2ac8b79fdc15fbe90884ffe534f
Weirdatfirst <weirdatfirst@gmail.com>
2019-03-14 15:19:48 -0400

    working on masscan autoscan

--- tools/WorkingOn/masscan_webui.sh
+++ tools/WorkingOn/masscan_webui.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
+#Read root password
+echo "What is your root password"
+read rootpassword
+
+#Read password for masscan setup
+echo "What password do you want to use for masscan"
+read masspassword
+
+#Install requirements
 apt-get update
 apt-get install -y apache2 php php-mysql mysql-server php-xml gcc make
 systemctl start mysql
 systemctl start apache2
+
+#Install and make Masscan
 path=$(pwd)
 masscan=/masscan/bin/masscan
 git clone https://github.com/robertdavidgraham/masscan
 cd masscan/
 make -j
-git clone https://github.com/offensive-security/masscan-web-ui
-mv masscan-web-ui/* /var/www/html/
+
+#Create symbolic link for masscan
 cd /usr/bin/
 ln -s $path$masscan masscan
 
-mysql -u root -ptoor <<EOF
+#Install Masscan Web UI
+git clone https://github.com/offensive-security/masscan-web-ui
+mv masscan-web-ui/* /var/www/html/
+
+#Add masscan user to mysql
+mysql -u root -p$rootpassword <<EOF
 create database masscan;
-CREATE USER 'masscan'@'localhost' IDENTIFIED BY 'changeme3';
+CREATE USER 'masscan'@'localhost' IDENTIFIED BY '$masspassword';
 GRANT ALL PRIVILEGES ON masscan.* TO 'masscan'@'localhost';
 exit
 EOF
+
 cd /var/www/html
-mysql -u root -ptoor masscan < db-structure-mysql.sql
+mysql -u root -p$rootpassword masscan < db-structure-mysql.sql
 
+#overwrite config file
+sed -i 's/pgsql/mysql/g' /var/www/html/config.php
+sed -i "s/changem3/$masspassword/g" /var/www/html/config.php
+
+#Explain how to use it
 echo "
-Then edit config.php
-
-#define('DB_DRIVER',	    'mysql');
-#define('DB_HOST',       'localhost');
-#define('DB_USERNAME',   'masscan');
-#define('DB_PASSWORD',   'changeme3');
-#define('DB_DATABASE',   'masscan');
-
 USAGE: masscan 10.0.0.0/8 -p80,21,53 --banners --source-ip 10.0.0.2 --max-rate 100000 -oX scan-01.xml
-
-Add To Database: php import.php scan-01.xml
-**************************************************************"
+Add To Database: php import.php scan-01.xml"
