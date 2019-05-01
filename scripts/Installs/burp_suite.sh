0bf3274045345643f1e73c32907a88933a807076
weirdatfirst <weirdatfirst@gmail.com>
2019-05-01 10:20:59 -0400

    adding gdrive to burp

--- scripts/Installs/burp_suite.sh
+++ scripts/Installs/burp_suite.sh
@@ -4,7 +4,7 @@ echo "What is you password for Burp suite zip"
 read password
 cd /opt/
 pip install gdown
-yes "" | gdown https://drive.google.com/uc?id=1k9KQNQmhZpobruPbjK5jFSfzrG95-bWt&export=download
+yes "" | gdown https://drive.google.com/uc?id=1k9KQNQmhZpobruPbjK5jFSfzrG95-bWt
 
 sleep 20
 
