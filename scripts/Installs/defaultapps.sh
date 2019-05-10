e03a0034b8a0c6a5e9c28563aed5752a9b947f8d
Weirdatfirst <weirdatfirst@gmail.com>
2019-05-10 13:16:53 -0400

    updating defaults

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Description: Default Applications
-sudo apt-get -y install copyq
-sudo apt-get -y install filezilla
-sudo apt-get -y install gnome-screenshot
-sudo apt-get -y install network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome
+sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome
+cd /usr/share/wordlists/
+git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/fuzzdb-project/fuzzdb.git
