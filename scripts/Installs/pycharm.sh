361194493c9c7c6d5b9bfe1dee6190a684ca54bb
weirdatfirst <weirdatfirst@gmail.com>
2019-06-12 12:57:09 -0400

    update pycharm

--- scripts/Installs/pycharm.sh
+++ scripts/Installs/pycharm.sh
@@ -2,7 +2,8 @@
 # Description: Pycharm
 pycharm=/pycharm-community-2018.2.4/bin/pycharm.sh
 cd /opt/
-wget www.weirdatfirst.com/files/tools/pycharm-community-2018.2.4.tar.gz
+pip install gdown
+gdown https://drive.google.com/open?id=1P8t9QTLw8681IUTqwC-QHtVzUJ0Jg-VT
 path=$(pwd)
 tar -xf pycharm-community-2018.2.4.tar.gz
 cd /usr/bin/
