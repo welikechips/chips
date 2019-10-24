2f2f9828456a4955550ed56f3eb320538c2470fa
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2019-10-24 11:57:15 -0400

    Update dirsearch_oneurl.sh

--- scripts/Recon/dirsearch_oneurl.sh
+++ scripts/Recon/dirsearch_oneurl.sh
@@ -3,3 +3,4 @@
 read -r -p "What is the url?" url
 command="dirsearch -u $url -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt -x 403"
 echo $command
+echo $command | xclip -sel clip
