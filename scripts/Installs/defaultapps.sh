6cbdd4469d2588f4632d41fb80effde5b1cf730d
weirdatfirst <weirdatfirst@gmail.com>
2019-12-11 19:45:10 -0500

    added openvpn support

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Description: Default Applications
 sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome
-sudo apt-get -y install network-manager-openconnect-gnome
+sudo apt-get -y install chromium network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome
 pip install jsbeautifier
 cd /usr/share/wordlists/
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
-apt-get install chromium -y
 echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --password-store=detect --no-sandbox --user-data-dir"' >> /etc/chromium.d/default-flags
