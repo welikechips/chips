64f763a7f6c086b6298c9b9aaf123f958d6bf8e6
Weirdatfirst <weirdatfirst@gmail.com>
2020-01-26 10:43:29 -0500

    Default installs

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Description: Default Applications
-sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome
+sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome exiftool
 sudo apt-get -y install chromium network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome
 pip install jsbeautifier
 cd /usr/share/wordlists/
