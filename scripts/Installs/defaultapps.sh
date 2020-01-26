ba049b8fa3c386c57b5b7a83588b0dc65e96d83c
Weirdatfirst <weirdatfirst@gmail.com>
2020-01-26 16:12:29 -0500

    Default installs

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -7,4 +7,3 @@ cd /usr/share/wordlists/
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
 echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --password-store=detect --no-sandbox --user-data-dir"' >> /etc/chromium.d/default-flags
-#
