0e3066f71066708271ef9584d1987a425089d531
weirdatfirst <weirdatfirst@gmail.com>
2020-03-16 11:44:54 -0400

    Adding masscan to recon scripts

---
+++ scripts/Recon/masscan_allports.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Description: Masscan-AllPorts - Scans all ports with Masscan
+path=$(pwd)
+filename=$path/targets.txt
+i=0
+
+if test -f $filename; then
+    echo "Found targets.txt"
+else
+    read -r -p 'Please enter full path of file with targets. ' filename
+fi
+
+
+read -r -p "Is this a cloud server? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        while read p; do
+            echo "Scanning" $p
+            masscan $p -p0-65535 --max-rate 1000000 -oX scan$i.xml
+            i=$[i+1]
+            sleep 60
+        done < $filename
+        ;;
+    *)
+        while read p; do
+            echo "Scanning" $p
+            masscan $p -p0-65535 -oX scan$i.xml
+            i=$[i+1]
+            sleep 60
+        done < $filename
+         ;;
+esac
