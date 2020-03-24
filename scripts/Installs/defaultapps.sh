5399fdeeb4b6dc0862647ff247d45d8fb26c027f
weirdatfirst <weirdatfirst@gmail.com>
2020-03-24 13:42:56 -0400

    Changed default fish setup

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Description: Default Applications
+# Description: Default Applications and Setup
 sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome exiftool
-sudo apt-get -y install chromium network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-vpnc network-manager-vpnc-gnome
+sudo apt-get -y install fish chromium network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-vpnc network-manager-vpnc-gnome
 pip install jsbeautifier
 cd /usr/share/wordlists/
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
+sudo echo 'set fish_greeting ""' >> /etc/fish/config.fish
 echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --password-store=detect --no-sandbox --user-data-dir"' >> /etc/chromium.d/default-flags
--- scripts/Installs/fish.sh
+++ scripts/Installs/fish.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-# Description: Fish Interactive Shell
+se#!/bin/bash
+# Description: Set Fish as default tmux shell
 apt-get update
 apt-get install fish
 sudo echo "set -g default-command /usr/bin/fish" >> ~/.tmux.conf
 sudo echo "set -g default-shell /usr/bin/fish" >> ~/.tmux.conf
-sudo echo 'set fish_greeting ""' >> /etc/fish/config.fish
+
 
