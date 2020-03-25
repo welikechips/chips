2eb4d9e13e3019a5b6db18471dcae96b7e4cc673
tnory56 <tnorberg@thomasnorberg.com>
2020-03-25 12:16:53 -0400

    adding simple httpserver upload module

---
+++ scripts/Installs/simpleHTTPserverWithUpload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Description: SimpleHTTPServerWithUpload 
+cd 
+wget https://gist.githubusercontent.com/tnory56/1e734b74c2cf64d9d9e86c36280bc4ad/raw/059aca1d510c615df3d9fedafabac4d538ebe352/SimpleHTTPServerWithUpload.py -P /usr/lib/python2.7/
