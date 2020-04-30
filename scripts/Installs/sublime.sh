3937a7251e06792312a42f7b9ed0e4c43806fd1f
tnory56 <tnorberg@thomasnorberg.com>
2020-04-30 11:22:31 -0400

    Update sublime.sh

--- scripts/Installs/sublime.sh
+++ scripts/Installs/sublime.sh
@@ -1,4 +1,4 @@
-ß#!/bin/bash
+#!/bin/bash
 # Description: Sublime
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
