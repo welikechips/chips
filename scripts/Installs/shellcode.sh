fd994ad43562cb2027afed058ebd397bc3dc9b45
tnory56 <tnorberg@thomasnorberg.com>
2020-04-30 11:24:42 -0400

    Update shellcode.sh

--- scripts/Installs/shellcode.sh
+++ scripts/Installs/shellcode.sh
@@ -6,4 +6,5 @@ cd tools
 git clone https://github.com/welikechips/shellcode.git
 path=$(pwd)
 ln -s ${path}/shellcode/shellcode.py /usr/bin/shellcode
-pip3 install pyperclip
+cd shellcode
+pip3 install -r requirements.txt
