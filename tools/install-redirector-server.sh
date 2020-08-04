81355d4a95236806363b3fde3a6d3d005a95f00a
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 18:05:10 -0400

    Create install-redirector-server.sh

---
+++ tools/install-redirector-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo su
+apt-get update -y && apt-get upgrade -y
+apt-get install apache2 -y
+a2enmod ssl rewrite proxy proxy_http
+a2ensite default-ssl.conf
+a2enmod proxy_connect
+service apache2 stop
+service apache2 start
+apt-get update -y
+apt-get install software-properties-common
+add-apt-repository universe
+add-apt-repository ppa:certbot/certbot
+apt-get update -y
+apt-get install certbot python-certbot-apache
