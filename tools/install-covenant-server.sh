6e634af9fe80702cdef41b9daedb6e080a75a1ed
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 16:19:07 -0400

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sudo su
-git clone https://github.com/welikechips/chips /root/chips
-ln -s /root/chips/menu.py /usr/local/bin/chips
-echo "2" | sudo /root/chips/scripts/Installs/bashrc.sh
-/root/chips/scripts/Installs/install-covenant.sh
-/root/chips/scripts/Installs/tmux.sh
+cd ~
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y git tmux curl tar zip 
+git clone https://github.com/welikechips/chips ~/chips
+ln -s ~/chips/menu.py /usr/local/bin/chips
+echo "2" | sudo ~/chips/scripts/Installs/bashrc.sh
+~/chips/scripts/Installs/install-covenant.sh
+~/chips/scripts/Installs/tmux.sh
 tmux new -s Covenant
-cd /root/Covenant/Covenant
+cd ~/Covenant/Covenant
+source ~/.bashrc
 dotnet run
