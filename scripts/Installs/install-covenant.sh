eaa194068655d63a376cc32202db34349ddcac11
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 14:57:37 -0400

    Update install-covenant.sh

--- scripts/Installs/install-covenant.sh
+++ scripts/Installs/install-covenant.sh
@@ -7,5 +7,3 @@ wget https://download.visualstudio.microsoft.com/download/pr/c1a30ceb-adc2-4244-
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.302-linux-x64.tar.gz -C $HOME/dotnet
 echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
 echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
-cat ~/.bashrc
-source ~/.bashrc
