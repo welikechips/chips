5241c4cfbb30d2e09a2e4d4a0d0ab736760418d0
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 17:01:34 -0400

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -5,7 +5,7 @@ git clone https://github.com/welikechips/chips ~/chips
 ln -s ~/chips/menu.py /usr/local/bin/chips
 echo "2" | sudo ~/chips/scripts/Installs/bashrc.sh
 ~/chips/scripts/Installs/install-covenant.sh
-~/chips/scripts/Installs/tmux.sh
+#~/chips/scripts/Installs/tmux.sh
 #tmux new -s Covenant
 #cd ~/Covenant/Covenant
 #source ~/.bashrc
