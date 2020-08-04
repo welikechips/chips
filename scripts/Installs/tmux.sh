b1ec8061fc030beb6fef71987d1ea4458d62b17a
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 17:46:19 -0400

    Update tmux.sh

--- scripts/Installs/tmux.sh
+++ scripts/Installs/tmux.sh
@@ -5,6 +5,6 @@ sudo git clone https://github.com/welikechips/.tmux.git
 sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo ln -s -f .tmux/.tmux.conf
 sudo cp .tmux/.tmux.conf.local .
-sudo gnome-terminal -- tmux new -s tmux-setup
+#sudo gnome-terminal -- tmux new -s tmux-setup
 echo "Press prefix + r to reload the tmux config"
 echo "Press prefix + I to install the plugins"
