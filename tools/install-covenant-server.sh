ef6f5b3715dd158a1076f11c11ca8ce1f849d68e
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 16:29:43 -0400

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 sudo su
 cd ~
-apt-get update
-apt-get dist-upgrade -y
-apt-get install -y git tmux curl tar zip 
 git clone https://github.com/welikechips/chips ~/chips
 ln -s ~/chips/menu.py /usr/local/bin/chips
 echo "2" | sudo ~/chips/scripts/Installs/bashrc.sh
