c8d1ba2c7da28ac82fe5a42d1b5c354f64168e23
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 16:57:44 -0400

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -6,7 +6,7 @@ ln -s ~/chips/menu.py /usr/local/bin/chips
 echo "2" | sudo ~/chips/scripts/Installs/bashrc.sh
 ~/chips/scripts/Installs/install-covenant.sh
 ~/chips/scripts/Installs/tmux.sh
-tmux new -s Covenant
-cd ~/Covenant/Covenant
-source ~/.bashrc
-dotnet run
+#tmux new -s Covenant
+#cd ~/Covenant/Covenant
+#source ~/.bashrc
+#dotnet run
