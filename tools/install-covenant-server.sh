a0af0254604381468d117f4c604156fc5564493c
tnory56 <tnorberg@thomasnorberg.com>
2020-08-05 12:33:24 -0400

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -7,4 +7,5 @@ wget https://download.visualstudio.microsoft.com/download/pr/c1a30ceb-adc2-4244-
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.302-linux-x64.tar.gz -C $HOME/dotnet
 echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
 echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
-tmux new-session -s covenant -d 'cd ~/Covenant/Covenant; dotnet run;'
+tmux new-session -s covenant -d 
+tmux send-keys -t covenant 'cd ~/Covenant/Covenant; dotnet run;' C-m
