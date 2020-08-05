d6441824cd5e55f37e6cbaae55ac237e19762d7e
tnory56 <tnorberg@thomasnorberg.com>
2020-08-04 23:35:44 -0400

    Update install-covenant.sh

--- scripts/Installs/install-covenant.sh
+++ scripts/Installs/install-covenant.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Description: Install Covenant
 
-cd
+sudo su
+cd ~
 git clone --recurse-submodules https://github.com/cobbr/Covenant
 wget https://download.visualstudio.microsoft.com/download/pr/c1a30ceb-adc2-4244-b24a-06ca29bb1ee9/6df5d856ff1b3e910d283f89690b7cae/dotnet-sdk-3.1.302-linux-x64.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.302-linux-x64.tar.gz -C $HOME/dotnet
