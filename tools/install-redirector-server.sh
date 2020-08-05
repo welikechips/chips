91fa8b66dcfbe95cc83ac93a8c4c85e3c71e9a4b
tnory56 <tnorberg@thomasnorberg.com>
2020-08-05 00:37:22 -0400

    Update install-redirector-server.sh

--- tools/install-redirector-server.sh
+++ tools/install-redirector-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo su
-apt-get update -y && apt-get dist-upgrade -y && apt-get autoremove -y
 apt-get install apache2 -y
 a2enmod ssl rewrite proxy proxy_http
 a2ensite default-ssl.conf
