49a23de501be05a0e6b8e782649d6f361d046edf
tnory56 <tnorberg@thomasnorberg.com>
2020-08-05 03:11:56 -0400

    Update replace_000_default.sh

--- tools/replace_000_default.sh
+++ tools/replace_000_default.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-public_ip=$1
+public_ip_raw=$1
 server_name=$2
+public_ip=$(echo "${public_ip_raw}" | sed "s/\./\\\\\\\\\\\\./g")
 
 sudo curl -sSL https://raw.githubusercontent.com/welikechips/chips/master/tools/000-default.conf \
-| sed 's|<REPLACE_PUBLIC_IP_ADDRESS>|$(echo "${public_ip}" | sed "s/\./\\\\\\\\\./g")|g' \
-| sed 's|<REPLACE_SERVER_NAME>|'""${server_name}'|g' > /etc/apache2/sites-enabled/000-default.conf
+| sed 's|<REPLACE_PUBLIC_IP_ADDRESS>|'"${public_ip}"'|g' \
+| sed 's|<REPLACE_SERVER_NAME>|'"${server_name}"'|g' > /etc/apache2/sites-enabled/000-default.conf
