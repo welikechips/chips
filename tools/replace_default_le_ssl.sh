e19a0022f0769d479a432fb6de3557793de768e1
tnory56 <tnorberg@thomasnorberg.com>
2020-08-05 02:48:32 -0400

    Update replace_default_le_ssl.sh

--- tools/replace_default_le_ssl.sh
+++ tools/replace_default_le_ssl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo curl -sSL https://raw.githubusercontent.com/welikechips/chips/master/tools/000-default-le-ssl.conf \
-| sed \"s/<REPLACE_PRIVATE_IP_ADDRESS>/$1/g" \
-| sed \"s/<REPLACE_SERVER_NAME>/$2/g\" \
-| sed \"s/<REPLACE_MIMIC_SERVER_NAME>/$3/g\" > /etc/apache2/sites-available/000-default-le-ssl.conf
+| sed "s/<REPLACE_PRIVATE_IP_ADDRESS>/$1/g" \
+| sed "s/<REPLACE_SERVER_NAME>/$2/g" \
+| sed "s/<REPLACE_MIMIC_SERVER_NAME>/$3/g" > /etc/apache2/sites-available/000-default-le-ssl.conf
