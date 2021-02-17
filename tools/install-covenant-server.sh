5138ba93091561ddf8cfced5f2e4ee90a34b35cc
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2021-02-17 15:25:41 -0500

    Update install-covenant-server.sh

--- tools/install-covenant-server.sh
+++ tools/install-covenant-server.sh
@@ -7,8 +7,8 @@ wget https://download.visualstudio.microsoft.com/download/pr/c1a30ceb-adc2-4244-
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.302-linux-x64.tar.gz -C $HOME/dotnet
 echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
 echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
-sudo tmux new-session -s covenant -d
-session=phishing-django
+session=covenant
+sudo tmux new-session -s ${session} -d
 window=${session}:0
 pane=${window}.0
-sudo tmux send-keys -t "${pane}" C-z "cd ~/Covenant/Covenant; dotnet run;" Enter
+sudo tmux send-keys -t "${pane}" C-z "cd ~/Covenant/Covenant; dotnet run;" ENTER
