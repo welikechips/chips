228274cfbba900672da69550bd283e4b1e46f98b
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2021-11-07 23:35:48 -0500

    Update tmux.sh

--- scripts/Installs/tmux.sh
+++ scripts/Installs/tmux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Description: Tmux
 cd 
-sudo git clone https://github.com/welikechips/.tmux.git
-sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo ln -s -f .tmux/.tmux.conf
-sudo cp .tmux/.tmux.conf.local .
+git clone https://github.com/welikechips/.tmux.git
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
 #sudo gnome-terminal -- tmux new -s tmux-setup
 echo "Press prefix + r to reload the tmux config"
 echo "Press prefix + I to install the plugins"
