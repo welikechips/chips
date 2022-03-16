f2cf552147ff4a2504485bbcb2ff836f81ec3f4f
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2022-03-16 11:39:28 -0400

    Update burp_suite.sh

--- scripts/Installs/burp_suite.sh
+++ scripts/Installs/burp_suite.sh
@@ -5,10 +5,10 @@ read password
 cd /opt/
 sudo apt-get -y install python3-pip
 pip3 install gdown
-gdown https://drive.google.com/uc?id=1t8VG0HGK30Bf6cdRSC6Yv7EVj_dmzEWr
+gdown https://drive.google.com/uc?id=1iyKNwf7cLSji4APRVzr6cAz0Ju-LHod9
 #wget www.weirdatfirst.com/files/burp.zip
 path=$(pwd)
 unzip -P "${password}" ${path}/burp.zip
 #removing until fix properly
 #rm ${path}/burp.zip
-java -jar ${path}/burp.jar
+sh ${path}/burp.sh
