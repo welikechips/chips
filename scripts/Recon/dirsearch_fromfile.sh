74cb6048fa3f3f71423496e200c806d588df7faa
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2022-05-24 10:25:27 -0400

    Update dirsearch_fromfile.sh
    Updated to new output switches

--- scripts/Recon/dirsearch_fromfile.sh
+++ scripts/Recon/dirsearch_fromfile.sh
@@ -7,9 +7,13 @@ else
     path=$(pwd)
     read -r -p 'Please enter full path of file ' file
     mkdir -p $path/dirsearch
+    cd dirsearch
+    touch tracker.txt
     name=$(cat "$file")
     for i in $name; do
-            echo https://$i
-            dirsearch -u http://$i -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt,asp,aspx -x 403 --simple-report=dirsearch/$i
+            counter=$((counter+1))
+            echo $i
+            echo "$i is in file $counter" >> tracker.txt 
+            dirsearch -u $i -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt,asp,aspx -x 403,302 -o $counter --format=json
     done
 fi
