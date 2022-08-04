9b52a60e5619b4ed2360aa9bef279b4805e7a9c1
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2022-08-04 09:31:30 -0400

    Update defaultapps.sh
    Adding tools from tomnomnom. Anew,gron,httprobe,waybackurls.

--- scripts/Installs/defaultapps.sh
+++ scripts/Installs/defaultapps.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Description: Default Applications and Setup
 sudo apt-get -y install gobuster steghide copyq ltrace strace filezilla gnome-screenshot network-manager-pptp network-manager-pptp-gnome network-manager-l2tp network-manager-l2tp-gnome exiftool
-sudo apt-get -y install dnsutils flameshot fish xclip python3-pip jd-gui chromium jxplorer network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-vpnc network-manager-vpnc-gnome
+sudo apt-get -y install dnsutils golang-go flameshot xclip python3-pip jd-gui chromium jxplorer network-manager-openconnect-gnome network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-vpnc network-manager-vpnc-gnome
 pip3 install jsbeautifier
 mkdir -p /usr/share/wordlists/
 cd /usr/share/wordlists/
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
 gem install evil-winrm
+go install -v github.com/tomnomnom/anew@latest
+go install github.com/tomnomnom/gron@latest
+go install github.com/tomnomnom/httprobe@latest
+go install github.com/tomnomnom/waybackurls@latest
 echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --password-store=detect --no-sandbox --user-data-dir"' >> /etc/chromium.d/default-flags
