ca5f167014ceebb2a94480263a2f6534eb6d91f1
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2023-03-11 09:17:07 -0500

    Delete sublime.sh
    Outdated.

--- scripts/Installs/sublime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Description: Sublime
-sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get -y install sublime-text
-git clone https://github.com/victorporof/Sublime-HTMLPrettify.git ~/.config/sublime-text-3/Packages/Sublime-HTMLPrettify
