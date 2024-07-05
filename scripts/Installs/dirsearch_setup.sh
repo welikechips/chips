74facea9e7102eca5182f8e085b371768cce5e06
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-07-05 18:33:42 -0400

    Delete scripts/Installs/dirsearch_setup.sh
    Old

--- scripts/Installs/dirsearch_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Description: DirSearch
-mkdir -p /root/tools
-cd /root/tools/
-git clone https://github.com/maurosoria/dirsearch.git
-cd /usr/bin/
-ln -s /root/tools/dirsearch/dirsearch.py dirsearch
