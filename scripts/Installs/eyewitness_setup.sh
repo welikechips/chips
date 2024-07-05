f8bab99b806c0c253268dabdff314b51f3dc2278
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-07-05 18:34:15 -0400

    Delete scripts/Installs/eyewitness_setup.sh
    Old

--- scripts/Installs/eyewitness_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Description: EyeWitness
-mkdir -p /root/tools/
-cd /root/tools
-git clone https://github.com/FortyNorthSecurity/EyeWitness
-cd /root/tools/EyeWitness/setup
-sh setup.sh -y
-cd /usr/bin/
-ln -s /root/tools/EyeWitness/EyeWitness.py eyewitness
