eb563b73156515ed176f0c0961e3e0c7879f38b2
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-07-05 19:12:32 -0400

    Update firefox-extensions.sh
    Fix errors

--- scripts/Installs/firefox-extensions.sh
+++ scripts/Installs/firefox-extensions.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Description: Install firefox extensions
-
-wget https://addons.mozilla.org/firefox/downloads/file/3687875/wappalyzer-6.5.20-fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1-an+fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3671736/hacktools-0.2.1-fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3522727/burner_emails_easy_fast_disposable_emails-0.1.41-an+fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3910047/modheader-3.1.27-fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3847514/cookiestxt-0.3-fx.xpi \
- https://addons.mozilla.org/firefox/downloads/file/3922535/simple_modify_headers-1.8.1-fx.xpi &&
+cd /tmp
+wget https://addons.mozilla.org/firefox/downloads/file/3687875/wappalyzer-6.5.20-fx.xpi 
+wget https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1-an+fx.xpi 
+wget https://addons.mozilla.org/firefox/downloads/file/3522727/burner_emails_easy_fast_disposable_emails-0.1.41-an+fx.xpi 
+wget https://addons.mozilla.org/firefox/downloads/file/3847514/cookiestxt-0.3-fx.xpi 
+wget https://addons.mozilla.org/firefox/downloads/file/3922535/simple_modify_headers-1.8.1-fx.xpi 
 firefox *.xpi 
 sleep 30
 rm *.xpi
