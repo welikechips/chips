b8f698b7fac7ef4d90e8028023f54f10ba21ce55
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-07-05 18:27:08 -0400

    Delete scripts/Installs/java8.sh
    Old

--- scripts/Installs/java8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Description: Java 8
-sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
