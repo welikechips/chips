d979bdcda33a2dc3027e1c8b52284a1e150eb4d9
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-07-05 18:24:47 -0400

    Update docker.sh
    changed to docker.io

--- scripts/Installs/docker.sh
+++ scripts/Installs/docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Description: Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get remove docker docker-engine docker.io -y
-apt-get install docker-ce -y
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
